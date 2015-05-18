89e4c992f8d458b26146c20f94ebe23934cb751f
Fumio SAGAWA <fumio.sagawa@gmail.com>
2015-05-18 10:55:59 +0900

    todoリストの構成見直し

--- app/components/todos/todos.jsx
+++ app/components/todos/todos.jsx
@@ -1,6 +1,6 @@
 var todoData = ['aaaa', 'cccc'];
 
-var Todo = React.createClass({
+var TodoList = React.createClass({
   propTypes: {
     // 削除するための処理をI/Fとして定義
     onDelete: React.PropTypes.func.isRequired
@@ -9,32 +9,15 @@ var Todo = React.createClass({
     this.props.onDelete(todo);
   },
   render: function() {
-    var todo = this.props.todo;
-    return (
-      <p className="input-group">
-        <input type="text" className="form-control" value={todo} />
-        <span className="input-group-btn">
-          <button className="btn btn-danger" onClick={ this.removeTodo.bind(this, todo) } aria-label="Remove">X</button>
-        </span>
-      </p>
-    );
-  }
-});
-
-var TodoList = React.createClass({
-  propTypes: {
-    // 削除するための処理をI/Fとして定義
-    onDelete: React.PropTypes.func.isRequired
-  },
-  deleteTodo: function (todo) {
-    this.props.onDelete(todo);
-  },
-  render: function() {
     var _this = this;
     var rows = this.props.todos.map(function(todo) {
       return (
-        <Todo todo={todo} onDelete={ _this.deleteTodo }></Todo>
-        // <Todo key={todo} todo={todo}></Todo>
+        <p key={todo} className="input-group">
+          <input type="text" className="form-control" value={todo} />
+          <span className="input-group-btn">
+            <button className="btn btn-danger" onClick={ _this.removeTodo.bind(this, todo) } aria-label="Remove">X</button>
+          </span>
+        </p>
       );
     });
     return (
